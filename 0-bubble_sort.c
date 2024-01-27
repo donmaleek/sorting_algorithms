22c296985fa54ba258d9bfc4657141e32b0e4cec
donmaleek <bamboloshari@gmail.com>
2024-01-27 10:56:03 -0800

    qn0 update on files

---
+++ 0-bubble_sort.c
@@ -0,0 +1,32 @@
+#include "sort.h"
+#include <stdlib.h>
+#include <stdio.h>
+/*
+ * bubble_sort - Function that sorts an array of ints
+ * @array - array of ints
+ * @size - size of ints
+ */
+void bubble_sort(int *array, size_t size)
+{
+	int temp;
+	size_t d, f;
+
+	if (!array || !size)
+		return;
+	d = 0;
+	while (d < size)
+	{
+		for (f = 0; f < size - 1; f++)
+		{
+			if (array(f) > array(f + 1))
+			{
+				temp = array(f);
+				array(f) = array(f + 1);
+				array(f + 1) = temp;
+				print_array(array, size);
+			}
+		}
+		d++;
+	}
+}
+
